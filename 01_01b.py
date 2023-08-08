edcaacddbf215ded6415dbb4971ce178668375fd
Onyema Ude <udeonyema@gmail.com>
2023-08-08 19:02:30 +0000

    Edited comment for count_words

--- 01_01b.py
+++ 01_01b.py
@@ -1,6 +1,7 @@
 def count_words(paragraph):
-    #code goes here
+    # code goes right here
     return
+
 
 def main():
     paragraph = """Nadia’s Garden Restaurant is the creation of husband and wife team Nadia and Timothy Arbore. 
@@ -11,7 +12,8 @@ def main():
      that could be made from local organic suppliers. Preservation of the building’s original layout has allowed them to create smaller, 
      more intimate, dining spaces. Nadia and Timothy are committed to sharing their family history of cuisine, along with their new inspirations,
       with their customers. Their passion for community, entertainment, and hospitality are found in every aspect of Nadia’s Garden Restaurant."""
-    print(count_words(paragraph)) 
+    print(count_words(paragraph))
+
 
 if __name__ == "__main__":
     main()
